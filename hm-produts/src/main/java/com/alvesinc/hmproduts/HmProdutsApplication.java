1b4b6e6397d3660c410355602c2751c3dadcef93
andrericardoalves <andrecajuru@gmail.com>
2021-03-12 10:20:58 -0300

    Project Produts created

---
+++ hm-produts/src/main/java/com/alvesinc/hmproduts/HmProdutsApplication.java
@@ -0,0 +1,13 @@
+package com.alvesinc.hmproduts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmProdutsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmProdutsApplication.class, args);
+	}
+
+}
---
+++ hm-produts/src/test/java/com/alvesinc/hmproduts/HmProdutsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvesinc.hmproduts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmProdutsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
